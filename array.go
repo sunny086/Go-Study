9b5ad2e0884b1ca3fb6f066934bc44c7d46ee1df
xujinshan <xujinshan0118@gmail.com>
2022-02-21 15:18:54 +0800

    array,strings

---
+++ array.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var arr = [5]int{1, 2, 3, 4, 5}
+	modifyArr1(arr)
+	fmt.Println(arr)
+	modifyArr2(&arr)
+	fmt.Println(arr)
+}
+
+func modifyArr1(a [5]int) {
+	a[1] = 20
+}
+
+func modifyArr2(a *[5]int) {
+	a[1] = 20
+}
