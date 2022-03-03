f2012374cdc3d56af24b89374d046a3ca7a308a7
xujinshan <xujinshan0118@gmail.com>
2022-03-03 14:13:59 +0800

    Gin form

---
+++ Test_Gin.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.POST("/form", func(c *gin.Context) {
+		types := c.DefaultPostForm("type", "13")
+		username := c.PostForm("username")
+		password := c.PostForm("userpassword")
+		// c.String(http.StatusOK, fmt.Sprintf("username:%s,password:%s,type:%s", username, password, types))
+		c.String(http.StatusOK, fmt.Sprintf("username:%s,password:%s,type:%s", username, password, types))
+	})
+	r.Run()
+}
+
+func getting(context *gin.Context) {
+	fmt.Println("123")
+}
