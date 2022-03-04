7a86d2fc36e945c1c69ba0411575eab5636198c2
xujinshan <xujinshan0118@gmail.com>
2022-03-04 11:10:46 +0800

    gin template

---
+++ template.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("tem/*")
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "template.html", gin.H{"title": "我是测试", "ce": "123456"})
+	})
+
+	r.Run()
+}
