aed715b8ab2cee26a7063be7907ea7b884019147
xujinshan <xujinshan0118@gmail.com>
2022-05-25 15:35:05 +0800

    gin 模板引擎

--- template.go
+++ template.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
-	r := gin.Default()
-	r.LoadHTMLGlob("tem/*")
-	r.GET("/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "template.html", gin.H{"title": "我是测试", "ce": "123456"})
-	})
+	//r := gin.Default()
+	//r.LoadHTMLGlob("tem/*")
+	//r.GET("/index", func(c *gin.Context) {
+	//	c.HTML(http.StatusOK, "template.html", gin.H{"title": "我是测试", "ce": "123456"})
+	//})
+	//
+	//r.Run()
 
+	r := gin.Default()
+	r.LoadHTMLGlob("tem/**/*")
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "user/index.html", gin.H{"title": "我是测试", "address": "www.5lmh.com"})
+	})
 	r.Run()
 }
