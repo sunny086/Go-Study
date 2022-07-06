f32e9f8be4b00c99ce94a8a0da9a9060f295a12a
xujinshan <xujinshan0118@gmail.com>
2022-07-06 14:04:20 +0800

    chan var wg sync.WaitGroup

--- chan.go
+++ chan.go
@@ -1,15 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
 
 func main() {
-	ch := make(chan int, 5)
+	ch := make(chan int, 10)
+	//ch = write(ch)
+	//close(ch)
+	//for i := 0; i < 6; i++ {
+	//	fmt.Println(<-ch)
+	//}
+	wg.Add(1)
+	go write(ch)
+	wg.Add(1)
+	go print(ch)
+	wg.Add(1)
+	go write(ch)
+	wg.Wait()
+}
+
+func write(ch chan int) {
 	for i := 0; i < 5; i++ {
 		ch <- 1
 	}
-	close(ch)
-	for i := 0; i < 6; i++ {
-		fmt.Println(<-ch)
-	}
 
+	for i := 0; i < 5; i++ {
+		time.Sleep(2 * time.Second)
+		ch <- i
+	}
+	wg.Done()
+
+}
+
+func print(ch chan int) {
+	for v := range ch {
+		fmt.Println(v)
+	}
+	wg.Done()
 }
