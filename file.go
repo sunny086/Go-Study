7ba7a452f09b7ae6ea454598104da94bcb74d148
xujinshan <xujinshan0118@gmail.com>
2022-07-15 16:51:47 +0800

    file read

--- file.go
+++ file.go
@@ -97,7 +97,30 @@ func main() {
 	//osStat()
 	//pathMkdir()
 	//fileRemove()
+	//readFile()
+}
 
+func readFile() {
+	f, err := os.OpenFile("D:\\test.txt", os.O_APPEND|os.O_RDWR, os.ModeAppend)
+	if err != nil {
+		fmt.Println("open file err: ", err)
+		return
+	}
+	defer f.Close()
+	readByte := make([]byte, 128) // 指定要读取的长度
+	for {
+		n, err := f.Read(readByte)       // 将数据读取如切片，返回值 n 是实际读取到的字节数
+		if err != nil && err != io.EOF { // 如果读到了文件末尾：EOF 即 end of file
+			fmt.Println("read file : ", err)
+			break
+		}
+
+		fmt.Println("read: ", string(readByte[:n]))
+		if n < 128 {
+			fmt.Println("read end")
+			break
+		}
+	}
 }
 
 func fileRemove() {
