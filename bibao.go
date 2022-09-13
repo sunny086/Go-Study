67ce9ee7273cb945cf3c19be5c2ad391a6af91eb
许金山 <xujinshan0118@gmail.com>
2022-09-14 00:31:26 +0800

    解决并发中的闭包：共享的环境变量作为函数参数传递

--- bibao.go
+++ bibao.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 	"sync"
 )
 
@@ -19,6 +18,7 @@ func main() {
 	//bibao_test1()
 
 	//bibao_test2()
+	bibao_test3()
 }
 
 //闭包测试1 闭包( Closure)在某些编程语言中也被称为 Lambda表达式（如Java） 函数+引用环境=闭包
@@ -31,8 +31,6 @@ func bibao_test1() {
 
 //闭包测试2 并发中的闭包
 func bibao_test2() {
-	gomaxprocs := runtime.GOMAXPROCS(runtime.NumCPU())
-	fmt.Println(gomaxprocs)
 	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
 		wg.Add(1)
@@ -40,6 +38,19 @@ func bibao_test2() {
 			fmt.Println(i)
 			wg.Done()
 		}()
+	}
+	wg.Wait()
+}
+
+//共享的环境变量作为函数参数传递
+func bibao_test3() {
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			fmt.Println(i)
+			wg.Done()
+		}(i)
 	}
 	wg.Wait()
 }
