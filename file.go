1b9b7c78596ea975f4a768749c20b1d5581cc524
xujinshan <xujinshan@wangtengtec.com>
2022-09-14 14:14:37 +0800

    软链接文件不能拷贝，会提示被拷贝的是目录

--- file.go
+++ file.go
@@ -108,6 +108,23 @@ func main() {
 func CopySoftLinkCommand() {
 	err2 := exec.Command("bash", "-c", "cp -d /opt/usb/soft_link_bak11 /opt/usb/soft_link_bak111").Run()
 	fmt.Println(err2)
+
+	//下面这种 直接copy会报错被拷贝的是个目录
+	/*	file, err := os.Open("/opt/usb/soft_link")
+		if err != nil {
+			fmt.Println(err)
+		}
+		create, err := os.Create("/opt/usb/soft_link_copy")
+		if err != nil {
+			fmt.Println(err)
+		}
+		written, err := io.Copy(create, file)
+		if err != nil {
+			//2022/08/13 16:14:39 read /opt/usb/soft_link: is a directory
+			log.Fatal(err)
+		}
+		log.Printf("Copied %d bytes.", written)*/
+
 }
 
 func removeExcludePath() {
