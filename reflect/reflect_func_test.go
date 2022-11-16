665e437f486278b8dac221e16882b7747fb20019
Gorilla <1043638938@qq.com>
2022-11-16 09:21:48 +0800

    反射执行函数注释

--- reflect/reflect_func_test.go
+++ reflect/reflect_func_test.go
@@ -10,6 +10,7 @@ func add(name string, age int) {
 	fmt.Printf("name is %s, age is %d \n", name, age)
 }
 
+// TestReflect_Func 反射执行函数
 func TestReflect_Func(t *testing.T) {
 	funcValue := reflect.ValueOf(add)
 	params := []reflect.Value{reflect.ValueOf("lisi"), reflect.ValueOf(20)}
