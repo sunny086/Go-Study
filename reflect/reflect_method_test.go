984cf118e44b7d496d85bf375b1a177b77357d14
Gorilla <1043638938@qq.com>
2022-11-16 12:24:10 +0800

    反射执行结构体的方法：无参有参

--- reflect/reflect_method_test.go
+++ reflect/reflect_method_test.go
@@ -7,27 +7,39 @@ import (
 )
 
 type user struct {
-	Name string
-	Age  int
+	Name  string
+	Age   int
+	Score int
 }
 
 func (u *user) ShowName() {
 	fmt.Println(u.Name)
 }
 
-func (u *user) AddAge(addNum int) {
-	fmt.Println("age add result:", u.Age+addNum)
+func (u *user) AddAge(age int) {
+	fmt.Println("age add result:", u.Age+age)
+}
+
+func (u *user) AddAgeAndScore(age, score int) {
+	u.Age = u.Age + age
+	u.Score = u.Score + score
+	fmt.Println("score add result:", u.Score+score)
+	fmt.Println("age add result:", u.Age+age)
 }
 
 func TestReflect_Method(t *testing.T) {
-	u := &user{"lisi", 20}
+	u := &user{"lisi", 20, 90}
 	v := reflect.ValueOf(u)
 	// 调用无参方法
 	methodV := v.MethodByName("ShowName")
 	methodV.Call(nil) // 或者传递一个空切片也可
-
 	// 调用有参方法
 	methodV2 := v.MethodByName("AddAge")
 	args := []reflect.Value{reflect.ValueOf(30)} //
 	methodV2.Call(args)
+	// 调用有参方法
+	methodV3 := v.MethodByName("AddAgeAndScore")
+	args2 := []reflect.Value{reflect.ValueOf(30), reflect.ValueOf(10)}
+	methodV3.Call(args2)
+	fmt.Println(v)
 }
