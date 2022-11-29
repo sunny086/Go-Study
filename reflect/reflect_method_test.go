fab2c1f0ea19cbfaf22cf0de62d014c103376cf1
Gorilla <1043638938@qq.com>
2022-11-29 11:29:47 +0800

    反射执行结构体方法，参数需要按顺序传递

--- reflect/reflect_method_test.go
+++ reflect/reflect_method_test.go
@@ -21,13 +21,15 @@ func (u *User) AddAge(age int) {
 	fmt.Println("age add result:", u.Age+age)
 }
 
-func (u *User) AddAgeAndScore(age, score int) {
+func (u *User) AddAgeAndScore(age, score int, str string) {
 	u.Age = u.Age + age
 	u.Score = u.Score + score
 	fmt.Println("score add result:", u.Score+score)
 	fmt.Println("age add result:", u.Age+age)
+	fmt.Println("str:", str)
 }
 
+// TestReflect_Execute_Method 执行结构体的方法
 func TestReflect_Execute_Method(t *testing.T) {
 	u := &User{"lisi", 20, 90}
 	v := reflect.ValueOf(u)
@@ -38,13 +40,14 @@ func TestReflect_Execute_Method(t *testing.T) {
 	methodV2 := v.MethodByName("AddAge")
 	args := []reflect.Value{reflect.ValueOf(30)} //
 	methodV2.Call(args)
-	// 调用有参方法
+	// 调用有参方法 参数需要按顺序传递
 	methodV3 := v.MethodByName("AddAgeAndScore")
-	args2 := []reflect.Value{reflect.ValueOf(30), reflect.ValueOf(10)}
+	args2 := []reflect.Value{reflect.ValueOf(30), reflect.ValueOf(10), reflect.ValueOf("调用有参方法 参数需要按顺序传递")}
 	methodV3.Call(args2)
 	fmt.Println(v)
 }
 
+// TestReflect_GetAllMethod 获取结构体的所有方法
 func TestReflect_GetAllMethod(t *testing.T) {
 	v := reflect.TypeOf(&User{})
 	// 获取所有方法
