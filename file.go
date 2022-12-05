6ed11ca163d738757884938f19d60d05287a6112
Gorilla <1043638938@qq.com>
2022-12-05 11:40:31 +0800

    os.flag清空创建追加的区别

--- file.go
+++ file.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"os"
+)
 
+func main() {
+	//err := ioutil.WriteFile("industry.rules", []byte(""), 0666)
+	//if err != nil {
+	//	panic(err)
+	//}
+	dstFile, err := os.OpenFile("industry.rules", os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	bufWriter := bufio.NewWriter(dstFile)
+	bufWriter.WriteString("alert tcp any any -> any any (msg:\"test\";sid:1000001;)")
+	bufWriter.Flush()
+	dstFile.Close()
 }
--- file/file_write_test.go
+++ file/file_write_test.go
@@ -12,8 +12,7 @@ import (
 // TestWriteFileByBuff 缓冲流写文件 最后要flush 如果不flush 会导致文件内容不全
 func TestWriteFileByBuff(t *testing.T) {
 	filepath := "./write_buff.txt"
-	//_ = ioutil.WriteFile(filepath, []byte(""), 0666)
-	dstFile, _ := os.OpenFile(filepath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModePerm)
+	dstFile, _ := os.OpenFile(filepath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, os.ModeAppend)
 	bufWriter := bufio.NewWriter(dstFile)
 	st := time.Now()
 	defer func() {
@@ -27,15 +26,10 @@ func TestWriteFileByBuff(t *testing.T) {
 	}
 }
 
-// TestWriteFileByString
+// TestWriteFileByString os.O_APPEND 会在文件末尾追加内容
 func TestWriteFileByString(t *testing.T) {
 	filePath := "./write_string.txt"
-	_, err := os.Create(filePath)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_RDWR, os.ModeAppend)
+	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_RDWR|os.O_CREATE, os.ModePerm)
 	if err != nil {
 		fmt.Println("open file err: ", err)
 		return
@@ -43,7 +37,7 @@ func TestWriteFileByString(t *testing.T) {
 	defer f.Close()
 
 	// 写入文件内容 按字符串写 WriteString()：
-	n, err := f.WriteString("xujinshan")
+	n, err := f.WriteString("xujinshan\n")
 	if err != nil {
 		fmt.Println("write err: ", err)
 		return
@@ -51,22 +45,17 @@ func TestWriteFileByString(t *testing.T) {
 	fmt.Println("write number = ", n)
 }
 
-// TestWriteFileByBytes
+// TestWriteFileByBytes os.O_TRUNC 会清空文件内容 os.O_CREATE 会创建文件
 func TestWriteFileByBytes(t *testing.T) {
 	filePath := "./write_byte.txt"
-	_, err := os.Create(filePath)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_RDWR, os.ModeAppend)
+	f, err := os.OpenFile(filePath, os.O_TRUNC|os.O_RDWR|os.O_CREATE, os.ModeAppend)
 	if err != nil {
 		fmt.Println("open file err: ", err)
 		return
 	}
 	defer f.Close()
 	// 写入文件内容 写入字节 Write()：
-	n, err := f.Write([]byte("123hello"))
+	n, err := f.Write([]byte("123hello\n"))
 	if err != nil {
 		fmt.Println("write err: ", err)
 		return
