0dc2f2e1afdd04e565ded4c88bc42e65f2f7128a
许金山 <xujinshan0118@gmail.com>
2022-12-07 00:38:49 +0800

    casbin

--- casbin/casbin_test.go
+++ casbin/casbin_test.go
@@ -7,8 +7,15 @@ import (
 )
 
 func TestCasbin01(t *testing.T) {
-	e, _ := casbin.NewEnforcer("casbin/model.conf", "casbin/policy.csv")
-	if b, _ := e.Enforce("alice", "data1", "read"); !b {
+	e, err := casbin.NewEnforcer("./model.conf", "./policy.csv")
+	if err != nil {
+		fmt.Println("casbin.NewEnforcer err:" + err.Error())
+	}
+	b, err := e.Enforce("alice", "data1", "read")
+	if err != nil {
+		fmt.Println("e.Enforce err:" + err.Error())
+	}
+	if !b {
 		t.Log("enforce failed")
 	} else {
 		t.Log("enforce success")
@@ -16,11 +23,10 @@ func TestCasbin01(t *testing.T) {
 }
 
 func TestCasbin02(t *testing.T) {
-	e, _ := casbin.NewEnforcer("/casbin/model.conf", "/casbin/policy.csv")
+	e, _ := casbin.NewEnforcer("./model.conf", "./policy.csv")
 	sub := "alice" // the user that wants to access a resource.
 	obj := "data1" // the resource that is going to be accessed.
 	act := "read"  // the operation that the user performs on the resource.
-
 	if res, _ := e.Enforce(sub, obj, act); res {
 		fmt.Println("enforce success")
 		// permit alice to read data1
