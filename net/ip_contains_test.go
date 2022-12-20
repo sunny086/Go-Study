6365b5b53eee8ac7630e7ff7f20daa29a74bbb3a
Gorilla <1043638938@qq.com>
2022-12-20 14:24:00 +0800

    两种api测试cidr包含关系： 使用net解析crdr返回的IpNet测试CIDR是否包含单个IP，入参是IP； 使用netip解析返回的prefix测试CIDR是否包含单个IP，入参是Addr

--- net/ip_contains_test.go
+++ net/ip_contains_test.go
@@ -2,10 +2,11 @@ package net
 
 import (
 	"net"
+	"net/netip"
 	"testing"
 )
 
-// 测试CIDR是否包含单个IP
+// 使用net解析crdr返回的IpNet测试CIDR是否包含单个IP，入参是IP
 func TestCidrContainsSingleIP(t *testing.T) {
 	ip := net.ParseIP("10.25.10.1")
 	t.Log(ip)
@@ -17,4 +18,24 @@ func TestCidrContainsSingleIP(t *testing.T) {
 	t.Log(ipNet.IP)           //10.25.10.0
 	t.Log(ipNet.Mask)         //ffffff00------->255.255.255.0
 	t.Log(ipNet.Contains(ip)) //true 子网掩码里面包含这个ip地址
+}
+
+// 使用netip解析返回的prefix测试CIDR是否包含单个IP，入参是Addr
+func TestCidrContainsSingleIP2(t *testing.T) {
+	// 解析cidr
+	prefix, err := netip.ParsePrefix("10.25.10.1/24")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(prefix)
+	// 解析ip
+	addr, err := netip.ParseAddr("10.25.10.1")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Log(addr)
+	contains := prefix.Contains(addr)
+	t.Log(contains) //true
 }
