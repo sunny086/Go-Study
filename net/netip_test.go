b2f172f11fa32b323f6709fbe9a22725ed483163
Gorilla <1043638938@qq.com>
2022-12-20 11:00:38 +0800

    netip.Prefix 可以判断子网掩码书写的ip是否是单个ip 换句话说是否是32后缀的

--- net/netip_test.go
+++ net/netip_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-// netip.ParsePrefix不能解析255.255.255.0这种后缀的掩码
+// netip.ParsePrefix 不能解析255.255.255.0这种后缀的掩码
 func TestParsePrefix1(t *testing.T) {
 	_, err := netip.ParsePrefix("10.25.10.1/255.255.255.0")
 	if err != nil {
@@ -13,11 +13,18 @@ func TestParsePrefix1(t *testing.T) {
 	}
 }
 
-// netip.ParsePrefix可以解析标准后缀的掩码，可以获取到掩码的长度和掩码的ip地址以及字符串形式的掩码
+// netip.ParsePrefix 可以解析标准后缀的掩码，可以获取到掩码的长度和掩码的ip地址以及字符串形式的掩码
 func TestParsePrefix2(t *testing.T) {
 	prefix, _ := netip.ParsePrefix("10.25.10.1/24")
 	t.Log(prefix)          //10.25.10.1/24
 	t.Log(prefix.Addr())   //10.25.10.1
 	t.Log(prefix.Bits())   //24
 	t.Log(prefix.String()) //10.25.10.1/24
+}
+
+// netip.Prefix 可以判断子网掩码书写的ip是否是单个ip 换句话说是否是32后缀的
+func TestPrefix1(t *testing.T) {
+	prefix, _ := netip.ParsePrefix("10.25.10.1/32")
+	ip := prefix.IsSingleIP()
+	t.Log(ip) //true
 }
