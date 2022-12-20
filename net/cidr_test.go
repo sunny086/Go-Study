f3545c12f724005f426f419adb0c9c870d0b5279
Gorilla <1043638938@qq.com>
2022-12-20 14:13:18 +0800

    使用netip.ParsePrefix解析cidr

--- net/cidr_test.go
+++ net/cidr_test.go
@@ -2,6 +2,7 @@ package net
 
 import (
 	"net"
+	"net/netip"
 	"strconv"
 	"strings"
 	"testing"
@@ -32,4 +33,14 @@ func TestParseCIDR1(t *testing.T) {
 	t.Log(cidr.Equal(ipNet.IP)) //false
 	t.Log(ipNet.IP.String())    //10.25.10.0--------->只要不是32，解析出来的都是0结尾的
 	t.Log(cidr)                 //10.25.10.1--------->我们发现parse解析出来的Ip是字符串里面的，但是IPNet里面的IP是0结尾的
+}
+
+// netip.ParsePrefix 解析子网掩码的另外一种方式 可以看一下
+// 可以看一下 net.netip_parse_prefix_test.go的测试
+func TestParseCIDR2(t *testing.T) {
+	prefix, _ := netip.ParsePrefix("10.25.10.1/24")
+	t.Log(prefix)          //10.25.10.1/24
+	t.Log(prefix.Addr())   //10.25.10.1
+	t.Log(prefix.Bits())   //24
+	t.Log(prefix.String()) //10.25.10.1/24
 }
