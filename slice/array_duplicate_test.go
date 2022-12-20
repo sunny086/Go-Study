0055413cabf39dce562aa70c751437d42d4545e5
Gorilla <1043638938@qq.com>
2022-12-20 18:21:08 +0800

    去重demo调整

--- slice/array_duplicate_test.go
+++ slice/array_duplicate_test.go
@@ -7,12 +7,16 @@ import (
 	"testing"
 )
 
+//https://www.jb51.net/article/244560.htm
+//还可以使用map进行去重
+
+// ---------------------------------------------------------------------------------------------------------------------
 // 测试去重
 func TestDataDuplicate(t *testing.T) {
 	strList := []string{"1", "1", "2", "2", "3", "3", "4", "4", "5", "5"}
-	t.Log(DataDuplicate(strList))           //5
-	t.Log(strList)                          //[1 2 3 4 5 3 4 4 5 5]
-	t.Log(strList[:DataDuplicate(strList)]) //[1 2 3 4 5 3 4 5]
+	t.Log(RemoveDuplicate1(strList))           //5
+	t.Log(strList)                             //[1 2 3 4 5 3 4 4 5 5]
+	t.Log(strList[:RemoveDuplicate1(strList)]) //[1 2 3 4 5 3 4 5]
 }
 
 // 测试去重 因为使用的是同一个地址引用 调用多次会导致数据变化
@@ -27,13 +31,13 @@ func TestDataDuplicate2(t *testing.T) {
 		"10.25.10.1/24",
 		"10.25.10.1/24",
 	}
-	t.Log(DataDuplicate(strList)) //第一次调用
+	t.Log(RemoveDuplicate1(strList)) //第一次调用
 	t.Log(strList)
-	t.Log(strList[:DataDuplicate(strList)]) //第二次调用，所以结果有问题
+	t.Log(strList[:RemoveDuplicate1(strList)]) //第二次调用，所以结果有问题
 }
 
 // 返回去重后的数据长度 最后一个元素的索引下标
-func DataDuplicate(strList []string) int {
+func RemoveDuplicate1(strList []string) int {
 	//如果是空切片，那就返回0
 	if len(strList) == 0 {
 		return 0
@@ -54,8 +58,14 @@ func DataDuplicate(strList []string) int {
 
 // ---------------------------------------------------------------------------------------------------------------------
 
+func TestDuplicate(t *testing.T) {
+	test := []string{"10.25.10.1", "10.25.10.11", "10.25.10.12", "10.25.10.12", "10.25.10.1", "10.25.10.11"}
+	duplicate := RemoveDuplicate2(test)
+	fmt.Println(duplicate)
+}
+
 // []string 去重 但是有一个bug 必须初始化一下这个string数组{""}
-func RemoveDuplicate(list []string) []string {
+func RemoveDuplicate2(list []string) []string {
 	// 这个排序很关键
 	sort.Strings(list)
 	i := 0
@@ -67,10 +77,4 @@ func RemoveDuplicate(list []string) []string {
 		}
 	}
 	return newlist
-}
-
-func TestDuplicate(t *testing.T) {
-	test := []string{"10.25.10.1", "10.25.10.11", "10.25.10.12", "10.25.10.12", "10.25.10.1", "10.25.10.11"}
-	duplicate := RemoveDuplicate(test)
-	fmt.Println(duplicate)
 }
