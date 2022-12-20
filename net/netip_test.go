cf7655578d3e8feb2042edbce0a7767f6e52f08e
Gorilla <1043638938@qq.com>
2022-12-20 10:53:36 +0800

    netip.ParsePrefix可以解析标准后缀的掩码，可以获取到掩码的长度和掩码的ip地址以及字符串形式的掩码

--- net/netip_test.go
+++ net/netip_test.go
@@ -12,3 +12,12 @@ func TestParsePrefix1(t *testing.T) {
 		t.Error(err) //netip.ParsePrefix("10.25.10.1/255.255.255.0"): bad bits after slash: "255.255.255.0"
 	}
 }
+
+// netip.ParsePrefix可以解析标准后缀的掩码，可以获取到掩码的长度和掩码的ip地址以及字符串形式的掩码
+func TestParsePrefix2(t *testing.T) {
+	prefix, _ := netip.ParsePrefix("10.25.10.1/24")
+	t.Log(prefix)          //10.25.10.1/24
+	t.Log(prefix.Addr())   //10.25.10.1
+	t.Log(prefix.Bits())   //24
+	t.Log(prefix.String()) //10.25.10.1/24
+}
