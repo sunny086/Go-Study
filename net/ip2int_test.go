46c30beac80af6e76b321edf988b3603512b1d37
Gorilla <1043638938@qq.com>
2022-12-20 18:22:30 +0800

    IP结构体和unit32互转

--- net/ip2int_test.go
+++ net/ip2int_test.go
@@ -2,6 +2,7 @@ package net
 
 import (
 	"encoding/binary"
+	"fmt"
 	"net"
 	"strconv"
 	"strings"
@@ -45,6 +46,47 @@ func Ip2Int2(ip string) int64 {
 	sum += int64(b1) << 16
 	sum += int64(b2) << 8
 	sum += int64(b3)
+
+	return sum
+}
+
+func TestIp2Int2(t *testing.T) {
+	// IP地址转换为uint32
+	IP1 := net.ParseIP("192.168.8.44")
+	IPUint32 := IPToUInt32(IP1)
+	fmt.Println(IPUint32)
+
+	// uint32转换为IP地址
+	IP2 := UInt32ToIP(IPUint32)
+	fmt.Println(IP2.String())
+}
+
+// UInt32ToIP uint32类型转换为IP
+func UInt32ToIP(intIP uint32) net.IP {
+	var bytes [4]byte
+	bytes[0] = byte(intIP & 0xFF)
+	bytes[1] = byte((intIP >> 8) & 0xFF)
+	bytes[2] = byte((intIP >> 16) & 0xFF)
+	bytes[3] = byte((intIP >> 24) & 0xFF)
+
+	return net.IPv4(bytes[3], bytes[2], bytes[1], bytes[0])
+}
+
+// IPToUInt32 IP转换为uint32类型
+func IPToUInt32(ipnr net.IP) uint32 {
+	bits := strings.Split(ipnr.String(), ".")
+
+	b0, _ := strconv.Atoi(bits[0])
+	b1, _ := strconv.Atoi(bits[1])
+	b2, _ := strconv.Atoi(bits[2])
+	b3, _ := strconv.Atoi(bits[3])
+
+	var sum uint32
+
+	sum += uint32(b0) << 24
+	sum += uint32(b1) << 16
+	sum += uint32(b2) << 8
+	sum += uint32(b3)
 
 	return sum
 }
