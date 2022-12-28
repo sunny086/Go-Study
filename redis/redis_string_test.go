997e096112a42e0a2a11082e8352b09939e08a88
Gorilla <1043638938@qq.com>
2022-12-28 15:10:37 +0800

    redis string test

--- redis/redis_string_test.go
+++ redis/redis_string_test.go
@@ -29,3 +29,17 @@ func TestString(t *testing.T) {
 	val := client.Get(context.Background(), "auxConfig:upperStorageLimit").Val()
 	t.Log("val:", val)
 }
+
+func TestStringArgs(t *testing.T) {
+	args := client.Get(context.Background(), "auxConfig:upperStorageLimit").Args()
+	t.Log("args:", args)
+}
+
+func TestStringScan(t *testing.T) {
+	var limit int64
+	err := client.Get(context.Background(), "auxConfig:upperStorageLimit").Scan(&limit)
+	if err != nil {
+		t.Log("err:", err)
+	}
+	t.Log("limit:", limit)
+}
