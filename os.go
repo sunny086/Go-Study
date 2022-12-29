8ef7b44f55c3ffd411bece0774b25072d3b24ab7
Gorilla <1043638938@qq.com>
2022-12-29 20:20:18 +0800

    go build之后可以运行加参数，代码中可以获取带命令行数据

--- os.go
+++ os.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+)
 
+func main() {
+	//go build之后可以运行加参数，下面的代码可以获取命令行参数
+	var config string
+	flag.StringVar(&config, "c", "abc", "choose config file.")
+	flag.Parse()
+	fmt.Println(config)
 }
