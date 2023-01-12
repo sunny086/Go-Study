068b8c9b4d6175682c42122179a72ff889b64131
Gorilla <1043638938@qq.com>
2023-01-12 10:45:47 +0800

    golang log

---
+++ log/log_test.go
@@ -0,0 +1,33 @@
+package log
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"testing"
+)
+
+// Go语言提供的基本日志功能
+func TestLog(t *testing.T) {
+	SetupLogger()
+	simpleHttpGet("www.google.com")
+	simpleHttpGet("http://www.google.com")
+	simpleHttpGet("baidu.com")
+	simpleHttpGet("www.baidu.com")
+	simpleHttpGet("http://www.baidu.com")
+}
+
+func SetupLogger() {
+	logFileLocation, _ := os.OpenFile("./test.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0744)
+	log.SetOutput(logFileLocation)
+}
+
+func simpleHttpGet(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Printf("Error fetching url %s : %s", url, err.Error())
+	} else {
+		log.Printf("Status Code for %s : %s", url, resp.Status)
+		resp.Body.Close()
+	}
+}
