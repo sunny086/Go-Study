5c0ba03ac0b866b24ab46b5a1fca97093f451802
Gorilla <1043638938@qq.com>
2023-03-23 15:09:29 +0800

    折线图xy轴名称测试

--- chart/go_chart_line_multi_test.go
+++ chart/go_chart_line_multi_test.go
@@ -18,18 +18,20 @@ func TimeArr() []time.Time {
 func TestMultiLineChart(t *testing.T) {
 	graph := chart.Chart{
 		XAxis: chart.XAxis{
+			Name:           "time",
 			TickPosition:   chart.TickPositionBetweenTicks,
 			ValueFormatter: chart.TimeValueFormatterWithFormat("2006-01-02 15:04:05"),
 		},
 
 		Series: []chart.Series{
 			chart.TimeSeries{
-				Name:    "并发数",
+				Name:    "conn",
+				Style:   chart.Style{ClassName: "true"},
 				XValues: TimeArr(),
 				YValues: []float64{133, 109, 123, 141, 143, 136, 115, 117, 117, 131, 108},
 			},
 			chart.TimeSeries{
-				Name:    "新建数",
+				Name:    "new",
 				YAxis:   chart.YAxisSecondary,
 				XValues: TimeArr(),
 				YValues: []float64{0, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1},
