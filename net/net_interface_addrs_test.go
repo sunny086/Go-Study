b87342ae4fffceb8e33fef5f2a1527d58454c601
Gorilla <1043638938@qq.com>
2023-04-11 15:11:49 +0800

    主机网卡的ip获取

---
+++ net/net_interface_addrs_test.go
@@ -0,0 +1,57 @@
+package net
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"testing"
+)
+
+func TestNetInterface(t *testing.T) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		t.Log(err)
+		return
+	}
+	for _, address := range addrs {
+		// 检查ip地址判断是否回环地址
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				fmt.Println(ipnet.IP.String())
+			}
+		}
+	}
+}
+
+func TestLocalIpv4(t *testing.T) {
+	localIp := getLocalIpV4()
+	log.Println("主机当前IPv4 ", localIp)
+}
+
+// getLocalIpV4 获取 IPV4 IP，没有则返回空
+func getLocalIpV4() string {
+	inters, err := net.Interfaces()
+	if err != nil {
+		panic(err)
+	}
+	for _, inter := range inters {
+		// 判断网卡是否开启，过滤本地环回接口
+		if inter.Flags&net.FlagUp != 0 && !strings.HasPrefix(inter.Name, "lo") {
+			// 获取网卡下所有的地址
+			addrs, err := inter.Addrs()
+			if err != nil {
+				continue
+			}
+			for _, addr := range addrs {
+				if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+					//判断是否存在IPV4 IP 如果没有过滤
+					if ipnet.IP.To4() != nil {
+						return ipnet.IP.String()
+					}
+				}
+			}
+		}
+	}
+	return ""
+}
