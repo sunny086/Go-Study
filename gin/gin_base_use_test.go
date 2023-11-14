a577bfcedc518771d3bf64f2ea4f2665b1527d47
Gorilla <1043638938@qq.com>
2023-11-14 11:28:49 +0800

    any类型测试

--- gin/gin_base_use_test.go
+++ gin/gin_base_use_test.go
@@ -6,15 +6,29 @@ import (
 	"testing"
 )
 
+type test struct {
+	A string
+	B any
+	C interface{}
+}
+
 func TestBaseUse(t *testing.T) {
+	var test test
+	test.A = "123"
+	test.B = "123"
+	test.C = "123"
+
 	r := gin.Default() //Default返回一个默认路由引擎
-	r.GET("/", func(c *gin.Context) {
+	r.GET("/hello", func(c *gin.Context) {
 		username := c.Query("username")
 		fmt.Println("username:", username)
 		c.JSON(200, gin.H{
 			"msg": "hello world",
+			"a":   test.A,
+			"b":   test.B,
+			"c":   test.C,
 		})
 	})
-	//http://127.0.0.1:8080/?username=123
+	//		http://127.0.0.1:8080/?username=123
 	r.Run()
 }
